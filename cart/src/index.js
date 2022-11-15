caa7274fa02878f247cf1ba66c00d401a2f8b27c
gabriel <gabrielrsp18@gmail.com>
2022-11-14 23:39:25 -0300

    async script loading

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,5 +1,1 @@
-import faker from 'faker'
-
-const carText = `<div>You have ${faker.random.number()} itens in your cart </div>`;
-
-document.querySelector('#cart-dev').innerHTML = carText;
+import('./bootstrap')
