e22a09f8dae51f0832e87dc09cbba9c51cf6206b
gabriel <gabrielrsp18@gmail.com>
2022-11-15 23:19:56 -0300

    add comments

--- products/src/index.js
+++ products/src/index.js
@@ -1,5 +1,7 @@
 import('./bootstrap'); //chamada assincrona do bootstrap
-/* 
+/*
+  basicamente esse é um escaneamento que verifica o que é necessário ser carregado nesse arvquivo antes de ser executado
+
   O import nesse formato de função irá carregar o bootstrap assincronamente
   permitindo o webpack identificar quais arquivos o codigo do index.js requer para rodar normalmente
 
